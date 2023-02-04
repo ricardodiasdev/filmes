c451ebace31c75408ed89e932699a96434717366
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-03 22:28:05 -0300

    Listando os filmes e CSS da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,46 @@
 import { useEffect, useState } from "react";
-import api from '../../services/api';
+import api from "../../services/api";
+import './home.css'
+
+import { Link } from "react-router-dom";
 
 // URL da api /movie/now_playing?api_key=c77e9ff67959672dd16a018876f5ab28&language=pt-BR
 const Home = () => {
-
-  const [filmes,setFilmes] = useState([]);
+  const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
-    async function loadFilmes () {
-      const response = await api.get('movie/now_playing', {
-        params:{
-          api_key:'c77e9ff67959672dd16a018876f5ab28',
-          language: 'pt-BR',
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "c77e9ff67959672dd16a018876f5ab28",
+          language: "pt-BR",
           page: 1,
-        }
-      })
+        },
+      });
 
-      console.log(response.data.results)
+      //console.log(response.data.results.slice(0,10))
+      setFilmes(response.data.results.slice(0, 10));
     }
 
-    loadFilmes()
-  }, [])
-  
+    loadFilmes();
+  }, []);
+
   return (
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="className lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.id}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 };
