c163b1dfb8254b17cb7b0c7e77519348f05c801e
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-05 10:34:21 -0300

    Detalhes do filme

--- src/pages/Film/index.js
+++ src/pages/Film/index.js
@@ -1,9 +1,73 @@
-import React from "react";
+import {useEffect, useState} from "react";
+
+import { useParams } from "react-router-dom";
+
+import './filme.info.css'
+
+
+import api from "../../services/api";
 
 const Film = () => {
+
+  const {id} = useParams();
+
+  const [filme, setFilme] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  const apiKey = process.env.REACT_APP_API_KEY
+  const language = process.env.REACT_APP_LANGUAGE
+
+
+  useEffect(() => {
+
+    const loadFilmePorId = async () => {
+      await api.get(`/movie/${id}`,{
+        params: {
+          api_key: apiKey,
+          language: language
+        }
+      })
+      .then((response)=> {
+        setFilme(response.data)
+        setLoading(false)
+      })
+      .catch(() => {
+        console.log("Filme não encontrado")
+      })
+    }
+
+    loadFilmePorId()
+
+    return () => {
+      console.log("Componente desmontado.")
+    }
+
+  }, [apiKey, language, id])
+
+  if(loading){
+    return(
+      <div className="filme-info">
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
+  
   return (
-    <div>
-      <h1>BEM VINDO A PÁGINA DETALHES DO FILME</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.id}
+      />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">Trailer</a>
+        </button>
+      </div>
     </div>
   );
 };
